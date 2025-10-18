ec5f46dc6eac4301d0e1738295b3d0d057c98f03
Svetlana <pannapc@mail.ru>
2025-10-18 21:00:36 +0300

    init commit

--- main.js
+++ main.js
@@ -60,3 +60,26 @@ console.log(typeof num);//number
 
 let bigNumber=Number("Хочется спать");
 console.log(bigNumber);//NaN преобразование не удалось
+
+//Логическое преобразование
+console.log(Boolean(""));//строка пустая, выводит false
+console.log(Boolean("0"));//строка с нулем-это true
+console.log(Boolean(" "));// пробел тоже true
+
+//неявное преобразование
+console.log('22'+8);//если один операнд является строкой,то второй также будет преобразован в строку
+console.log(15-'3');//12, '3' приводится к числу
+console.log('4'/'2');//2, обе строки проводятся к числу
+
+//Операторы сравнения
+console.log(35<66);//true
+console.log(25>=26);//false
+console.log(6==8);//false
+console.log(9!=9);//false
+console.log('Л'>'Р');//false
+console.log('A'<'v');//true, т.к.А(U+0041),а v(U+0076)
+console.log('Гигант'>'Лилипут');//false
+console.log('5'<9);//true. Строка '5' приводится к числу 5
+console.log('006'==7);//false, строка '006' приводится к числу 6, а 6 не равно 7
+console.log(8===8);//true
+console.log(1===0);//false
